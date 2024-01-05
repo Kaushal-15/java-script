5e3285f5339c82574c0c48f1387816653ec2b70c
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-01-05 19:59:29 +0530

    data types

---
+++ data types.js
@@ -0,0 +1,25 @@
+//numbers
+
+alert( isNaN(NaN) ); 
+alert( isNaN("str") ); 
+
+alert( Number.isFinite(123) ); 
+alert( Number.isFinite(Infinity) ); 
+alert( Number.isFinite(2 / 0) ); 
+
+
+alert( Number.isFinite("123") ); 
+alert( isFinite("123") );
+
+alert(-'250ka');
+alert(parseInt('250inr'));
+alert(parseFloat('150.34.55'));
+
+alert(parseInt('24.938'));
+alert(parseInt('250a34'));
+alert(Math.round());
+
+alert(Math.max(1,4,5,66,677));
+alert(Math.min(1393,3878,3389,339));
+
+alert(Math.pow(2, 12));
