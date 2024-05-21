d999662d3c64393f2a1a75106893f8b459e13db7
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-05-21 16:31:36 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,8 @@
+//variables can be assigned using var , let , const//
+let firstName = "kaushal";
+firstName = "sanjay";
+console.log(firstName);
+const chrimstas = "december";//we cannot re-assign the value when given const//
+let number=3;
+console.log(number);
+console.log(chrimstas);
