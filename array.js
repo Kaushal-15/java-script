42bd00db3016f2482636a84ffa9f68e5d9d83286
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-05-22 18:41:06 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,10 @@
+let productColors = ['blue','green','red'];
+console.log(productColors);
+console.log(productColors[0]); //here key is blue or data and key is index value//
+console.log(typeof productColors);
+console.log(productColors.length);
+
+let Num= ['one',2,'three','four',5,6,'seven'];
+console.log(Num[2]);
+console.log(Num.length);
+console.log(typeof productColors);
