74a71d019ffa21b0d41699045bd2feef75088490
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-05-24 19:45:15 +0530

    added files

---
+++ string_concation.js
@@ -0,0 +1,15 @@
+function sayhi(name){
+    console.log("hi" + name);
+}
+
+sayhi("sanjay");
+
+function multiply(num1,num2) {
+    return(num1*num2)//if we do not give return it will be undefined//
+}
+console.log(multiply(num1,num2));
+
+function total(sub1,sub2,sub3,sub4,sub5) {
+    return (sub1+sub2+sub3+sub4+sub5);
+}
+console.log(total(35,55,55,78,0));
