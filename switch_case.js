c9f50628ec259407940d952fade60c8bcb125564
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-06-04 14:12:00 +0000

    file added

---
+++ switch_case.js
@@ -0,0 +1,15 @@
+let job = "Cloud Engineer";
+switch (job) {
+    case "software developer":
+        console.log("He writes the code");
+        break;
+case "Designer":
+  console.log("He makes interactive UI and UX desings");
+break;
+case "Cloud Engineer":
+    console.log("He deploys and updates it to the cloud");
+    break;
+    default  :
+    console.log("he speaks to the customers");
+        break;
+}
