d033322beeee3a847087afdbcf1758ae63cfd921
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-06-04 14:27:25 +0000

    file added

---
+++ for_loops.js
@@ -0,0 +1,12 @@
+let numbers = [ 1,2,3,4,5,6,7,8,9,10];
+
+let lengthOfArray = numbers.length;
+
+for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index]);  
+}
+
+for (let num = 0; num <=10; num++){
+ console.log(num);
+
+}
