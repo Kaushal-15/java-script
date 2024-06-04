0bbec51de9f9bb03d01f67e355148fdbe6b51878
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-06-04 12:17:29 +0000

    files added

---
+++ if-else.js
@@ -0,0 +1,19 @@
+let PriceOfchocolate = 2.95;
+let AmountInCash = 5;
+const CanBuyChocolate = AmountInCash >= PriceOfchocolate;
+//console.log(CanBuyChocolate);//
+
+if (CanBuyChocolate) {
+    console.log("Thankyou for your purchase,Have a great day ahead");
+}else{
+    console.log("Sorry u cannot afford this product");
+}
+
+let num = 4 ;
+
+if (num>=18) {
+    console.log("Congratulations you can Vote!");
+
+} else if (num<=18) {
+    console.log("Sorry u cannot Vote");
+}
