2f321bc9cf9198b3e5d69d7e18b7533b200de8e5
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-06-10 14:56:19 +0000

    file added

--- switch_case.js
+++ switch_case.js
@@ -13,3 +13,18 @@ case "Cloud Engineer":
     console.log("he speaks to the customers");
         break;
 }
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too big' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
