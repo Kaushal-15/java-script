dcf9edbcde16f709a69ec80ba389d667fa834cf7
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-06-10 13:34:07 +0000

    file added

--- while_loop.js
+++ while_loop.js
@@ -1,23 +1,30 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7.8, 8];
-let idx = 0;
-while (idx < numbers.length) {
-    console.log(numbers[idx]);
-    idx++;
-}
+// let numbers = [1, 2, 3, 4, 5, 6, 7.8, 8];
+// let idx = 0;
+// while (idx < numbers.length) {
+//     console.log(numbers[idx]);
+//     idx++;
+// }
 
-let sum = 0;
-while (true) {
-    console.log("loop");
-    sum++;
+// let sum = 0;
+// while (true) {
+//     console.log("loop");
+//     sum++;
 
-    if (sum === 10) {
-        break;
-    }
-}
-//do while loops//
-let i = 0;
-do {
-    console.log(i)
-    i++
+//     if (sum === 10) {
+//         break;
+//     }
+// }
+// //do while loops//
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++
     
-} while (i < 10);
+// } while (i < 10);
+
+let value = prompt("what is the official name of JavaScript?");
+if (value == "ECMAScript") {
+    ("ya! its correct");
+} else {
+    console.log("The official language of JavaScript is ECMAScript");
+}
