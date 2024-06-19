6ad7dbda30aef64d6afe757843fd6ed75f25f9c3
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-06-19 19:50:28 +0530

    file added

--- objects.js
+++ objects.js
@@ -30,3 +30,11 @@ const person = {
 };
 
 console.log(person);
+
+let user = { name: 'John' };
+
+let admin = user;
+
+admin.name = 'Pete'; // changed by the "admin" reference
+
+alert(user.name); // 'Pete', changes are seen from the "user" reference
