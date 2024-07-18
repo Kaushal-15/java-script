e0846eb512bb16074bfe6d0f25e428635c2f5e6b
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-07-18 19:22:04 +0530

    file added

---
+++ elseif_ladder.js
@@ -0,0 +1,20 @@
+let day = prompt();
+
+if (day == 1) {
+  alert("MONDAY");
+} else if (day == 2) {
+  alert("TUESDAY");
+} else if (day == 3) {
+  alert("WEDNESDAY");
+} else if (day == 4) {
+  alert("THURSDAY");
+} else if (day == 5) {
+  alert("FRIDAY");
+} else if (day == 6) {
+  alert("SATURDAY");
+} else if (day == 7) {
+  alert("SUNDAY");
+} else {
+  alert("Enter proper number between 1 to 7");
+}
+
