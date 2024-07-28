9d75626f8c8d3286589570421a24390d486ecdec
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-07-28 18:10:43 +0530

    file added

--- function1.js
+++ function1.js
@@ -9,3 +9,11 @@ function sum(x,y=10){
 }
 sum(10);//in default parameter if no value is passed it will reflect if value is passed it will passed//
 sum(10,15);
+//return//
+//return statement doesnt allow anyone code to execute after the reeturn keyword //
+function sum(a,b=10) {
+    return a*b;
+    console.log("hello");//here this hello log message will be not logged due to return keyword//
+}
+let result = sum(30,20);
+console.log("the result is:" + result);
