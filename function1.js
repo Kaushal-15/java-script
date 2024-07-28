726447ac5c2509d636c5c82dfb5f721299315d02
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-07-28 18:03:40 +0530

    file added

--- function1.js
+++ function1.js
@@ -3,3 +3,9 @@ function hello(first,name) {
 
 }
 hello("elon","musk");
+//default parameter//
+function sum(x,y=10){
+    console.log(x+y)
+}
+sum(10);//in default parameter if no value is passed it will reflect if value is passed it will passed//
+sum(10,15);
